4c28e69e4db6593471bfe87631b8fafef2c92e7d
gauravchaturvedi <gkchat10@gmail.com>
2018-03-12 17:41:50 -0400

    Removed commented out code

--- index.js
+++ index.js
@@ -13,32 +13,9 @@ http.createServer(function (req, res) {
     form.parse(req, function (err, fields, files) {
       console.log(files.filetoupload.name);
       var oldpath = files.filetoupload.path;
-
-      // var options = {
-      //   sheet:'1',
-      //   isColOriented: false,
-      //   omitEmtpyFields: false
-      // }
-      // convertExcel(oldpath, 'row.json', options, (err, data) => {
-      //   if(err) {
-      //     console.log( "JSON conversion failure: #{err}");
-      //     return err;
-      //   } else {
-      //     console.log('success');
-      //     res.write(JSON.stringify(data));
-      //     return res.end();
-      //   }
-      // });
-
       console.log(process.cwd());
       console.log(files.filetoupload.name);
       var newpath = process.cwd() + '/' + files.filetoupload.name;
-      // fs.rename(oldpath, newpath, function (err) {
-      //   if (err) throw err;
-      //   res.write('File uploaded and moved!');
-      //   res.end();
-      // });
-
 
       fs.readFile(oldpath, function (err, data) {
             if (err) throw err;
