fd58d4d68696f86be8b0d83e7a5e7a3f0590abad
gauravchaturvedi <gkchat10@gmail.com>
2018-03-26 04:56:52 -0400

    Added express and also authentication

--- index.js
+++ index.js
@@ -1,80 +1,158 @@
-var http = require('http');
+const express = require('express');
+const app = express();
+var path = require('path');
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+const cors = require('cors');
 var formidable = require('formidable');
 var fs = require('fs');
 var convertExcel = require('excel-as-json').processFile;
+require('dotenv').config();
 
-var port = process.env.PORT || 8080;
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
+}
 
-http.createServer(function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      console.log(files.filetoupload.name);
-      var oldpath = files.filetoupload.path;
-      console.log(process.cwd());
-      console.log(files.filetoupload.name);
-      var newpath = process.cwd() + '/' + files.filetoupload.name;
+app.use(cors());
 
-      fs.readFile(oldpath, function (err, data) {
-            if (err) throw err;
-            console.log('File read!');
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
 
-            // Write the file
-            fs.writeFile(newpath, data, function (err) {
-                if (err) throw err;
-                res.write('Awesome.. file uploaded!');
-                res.end();
-                console.log('File written!');
-            });
+  // Validate the audience and the issuer.
+  aud: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
 
-            // Delete the file
-            fs.unlink(oldpath, function (err) {
-                if (err) throw err;
-                console.log('File deleted!');
-            });
-        });
+const checkScopes = jwtAuthz(['read:messages']);
 
-    });
-  } else if(req.url == '/getData') {
-    var options = {
-      sheet:'3',
-      isColOriented: false,
-      omitEmtpyFields: true
-    }
-    convertExcel('Citi FinDex Project Master Sheet_Final.xlsx', 'row.json', options, (err, data) => {
-      if(err) {
-        console.log( "JSON conversion failure: #{err}");
-        return err;
-      } else {
-        console.log('success');
-        res.write(JSON.stringify(data));
-        return res.end();
-      }
-    })
-  } else if(req.url == '/getPercentileData') {
-    var options = {
-      sheet:'5',
-      isColOriented: false,
-      omitEmtpyFields: true
-    }
-    convertExcel('Citi FinDex Project Master Sheet_Final.xlsx', 'row.json', options, (err, data) => {
-      if(err) {
-        console.log( "JSON conversion failure: #{err}");
-        return err;
-      } else {
-        console.log('success');
-        res.write(JSON.stringify(data));
-        return res.end();
-      }
-    })
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+console.log('blah');
+
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+
+app.post('/fileupload', function(req, res) {
+
+  console.log('Posting file now');
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    console.log(files.filetoupload.name);
+    var oldpath = files.filetoupload.path;
+    console.log(process.cwd());
+    console.log(files.filetoupload.name);
+    var newpath = process.cwd() + '/' + files.filetoupload.name;
+
+    fs.readFile(oldpath, function (err, data) {
+          if (err) throw err;
+          console.log('File read!');
+
+          // Write the file
+          fs.writeFile(newpath, data, function (err) {
+              if (err) throw err;
+              res.json({
+                message: 'Awesome.. file uploaded!'
+              });
+              console.log('File written!');
+          });
+
+          // Delete the file
+          fs.unlink(oldpath, function (err) {
+              if (err) throw err;
+              console.log('File deleted!');
+          });
+      });
+
+  });
+
+});
+
+
+app.get('/getData', checkJwt, function(req, res) {
+
+  var options = {
+    sheet:'3',
+    isColOriented: false,
+    omitEmtpyFields: true
   }
-}).listen(port);
+  convertExcel('Citi FinDex Project Master Sheet_Final.xlsx', 'row.json', options, (err, data) => {
+    if(err) {
+      console.log( "JSON conversion failure: #{err}");
+      return err;
+    } else {
+      console.log('success');
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data));
+      return res.end();
+    }
+  })
+
+});
+
+app.get('/getPercentileData', checkJwt, function(req, res) {
+
+  var options = {
+    sheet:'5',
+    isColOriented: false,
+    omitEmtpyFields: true
+  }
+  convertExcel('Citi FinDex Project Master Sheet_Final.xlsx', 'row.json', options, (err, data) => {
+    if(err) {
+      console.log( "JSON conversion failure: #{err}");
+      return err;
+    } else {
+      console.log('success');
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data));
+      // return res.end();
+    }
+  })
+
+});
+
+app.get('/getPercentileData', checkJwt, function(req, res) {
+
+  var options = {
+    sheet:'5',
+    isColOriented: false,
+    omitEmtpyFields: true
+  }
+  convertExcel('Citi FinDex Project Master Sheet_Final.xlsx', 'row.json', options, (err, data) => {
+    if(err) {
+      console.log( "JSON conversion failure: #{err}");
+      return err;
+    } else {
+      console.log('success');
+      res.write(JSON.stringify(data));
+      // return res.end();
+    }
+  })
+});
+
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  return res.status(err.status).json({ message: err.message });
+});
+
+app.listen(process.env.PORT || 8080);
+console.log('Listening on http://localhost:8080');
